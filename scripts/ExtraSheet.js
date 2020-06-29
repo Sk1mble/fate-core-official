3818d48234765af74e654076a0df77b105ad0e33
Sk1mble <rbellingham@gmail.com>
2020-06-29 11:24:07 +0100

    Fixed a bug where opening the tab on one character sheet opened the tab on another.

--- scripts/ExtraSheet.js
+++ scripts/ExtraSheet.js
@@ -1,20 +1,38 @@
-import { ItemSheetFATE } from "./ItemSheet.js";
-export class ExtraSheet extends ItemSheetFATE {
-    static get defaultOptions() {
-        const options = super.defaultOptions;
-        options.height = 600;
-        return options;
-    }
+export class ExtraSheet extends ItemSheet {
     getData() {
         const data = super.getData();
-        const actions = {};
-        for (const [action, value] of Object.entries(data.data.actions)) {
-            actions[action] = {
-                label: game.i18n.localize(`FATE.Actions.${action}`),
-                value,
-            };
+        data.type = this.item.type.toLowerCase();
+        data.dataTemplate = () => `systems/ModularFate/templates/ExtraSheet.html`;
+
+        console.log(data.actor);
+        if (data.data.box_values == undefined){
+            //data.actor.updateEmbeddedEntity()
         }
-        data.actions = actions;
+        console.log(this.object.actor);
+        //console.log(data.data.box_values.length);
+
         return data;
+    }
+
+    get template (){
+        return 'systems/ModularFate/templates/ExtraSheet.html';
+    }
+    activateListeners(html){
+    }
+
+    async _on_boxes_change(html, event){
+        console.log(event.target.value)
+        let num = parseInt(event.target.innerHTML);
+        console.log(num);
+    }
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.classes = options.classes.concat(['fate', 'item']);
+        options.width = 500;
+        options.height = "auto";
+        options.resizable = true;
+        options.submitOnChange = true;
+        options.title=`Extra: ${this.name}`
+        return options;
     }
 }
