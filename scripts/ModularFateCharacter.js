b0412714b0f8925994c9ad0484658c5e1701943b
Sk1mble <rbellingham@gmail.com>
2020-07-15 22:47:41 +0100

    Visual tweaks; fixed track aspect boxes in FateUtilities

--- scripts/ModularFateCharacter.js
+++ scripts/ModularFateCharacter.js
@@ -17,7 +17,7 @@ export class ModularFateCharacter extends ActorSheet {
     static get defaultOptions() {
         const options = super.defaultOptions;
         options.width = "860"
-        options.height = "920"
+        options.height = "950"
         options.scrollY = ["#skills_body", "#aspects_body","#tracks_body", "#stunts_body", "#biography_body"]
         mergeObject(options, {
             tabs: [
