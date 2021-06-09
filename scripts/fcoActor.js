7f2c08edc800044a5a1bba2519c712d36c108cc4
Sk1mble <rbellingham@gmail.com>
2021-06-09 14:12:56 +0100

    Update fcoActor.js
    Fix bug occurring on deletion of extras.

--- scripts/fcoActor.js
+++ scripts/fcoActor.js
@@ -468,7 +468,7 @@ export class fcoActor extends Actor {
         await actor.update(updateObject);
         // This is required in order to make sure we get the combined skills setup correctly
         for (let extra of actor.items){
-            if (extra.data.data.active) await actor.updateFromExtra(extra);
+            if (extra.id != item.id && extra.data.data.active) await actor.updateFromExtra(extra);
         }
         let ctracks = duplicate(actor.data.data.tracks);
         let cskills = duplicate(actor.data.data.skills);
