b1f8d3b88c1935c3caab2145b3073c647cd1948b
Sk1mble <rbellingham@gmail.com>
2024-05-23 21:57:28 +0100

    v4.7.2
    * Very minor release - added a workaround to ensure drawings on the canvas representing aspects update when their name changes or number of free invokes is changed. Also updated the system manifest to have the correct format for grid units.

--- scripts/FateUtilities.js
+++ scripts/FateUtilities.js
@@ -655,12 +655,15 @@ class FateUtilities extends Application{
             }
             let height = size * 2;
             let width = (text.length * size) / 1.5;
-            drawing.document.update({
+            await drawing.document.update({
                 "text":text,
                 width: width,
                 height: height,
                 fontFamily: font,
             });
+            await setTimeout(async () => {
+                canvas.drawings.draw(); //TEMP: Required due to a core bug    
+            }, 50);
         }
 
         game.scenes.viewed.setFlag("fate-core-official", "situation_aspects",aspects);
@@ -1146,6 +1149,9 @@ class FateUtilities extends Application{
                     if (bonus > 0){
                         if (game.scenes.viewed){ 
                             await game.scenes.viewed.updateEmbeddedDocuments("Drawing", updates);
+                            await setTimeout(async () => {
+                                canvas.drawings.draw(); //TEMP: Required due to a core bug    
+                            }, 50);
                             flavor = `<br>${game.i18n.localize("fate-core-official.FreeInvokes")} +${bonus} (${aspectsInvoked.join(", ")})`
                             await game.scenes.viewed.setFlag("fate-core-official", "situation_aspects", all_sit_aspects);
                         }
@@ -1275,6 +1281,9 @@ class FateUtilities extends Application{
                             flavor += ` (${sit_aspect.name})`
                             if (game.scenes.viewed){
                                 await game.scenes.viewed.updateEmbeddedDocuments("Drawing", updates);
+                                await setTimeout(async () => {
+                                    canvas.drawings.draw(); //TEMP: Required due to a core bug    
+                                }, 50);
                                 await game.scenes.viewed.setFlag("fate-core-official", "situation_aspects", all_sit_aspects);    
                             }
                             invokedAspect = sit_aspect.name;
@@ -1714,6 +1723,9 @@ class FateUtilities extends Application{
                 height: height,
                 fontFamily: font,
             });
+            await setTimeout(async () => {
+                canvas.drawings.draw(); //TEMP: Required due to a core bug    
+            }, 50);
         }
     }
 
@@ -1970,12 +1982,15 @@ class FateUtilities extends Application{
             }
             let height = size * 2;
             let width = (text.length * size) / 1.5;
-            drawing.document.update({
+            await drawing.document.update({
                 "text":text,
                 width: width,
                 height: height,
                 fontFamily: font,
             });
+            await setTimeout(async () => {
+                canvas.drawings.draw(); //TEMP: Required due to a core bug    
+            }, 50);
         }
     }
 
